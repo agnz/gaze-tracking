071cc6db147b93c9f3b2913a9d6df3f31910296a
Yiming Zhang <yiz637@ucsd.edu>
2018-08-08 12:12:51 -0700

    added alarm detection and improved timing

--- audio-det/audio_stream.py
+++ audio-det/audio_stream.py
@@ -5,98 +5,112 @@ import numpy as np
 import time
 
 #initialize global variables
-CHUNK = 1024
+CHUNK = 2048
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-MAX_KNOCK_TIME = 1 #time gap between each knock to be sequential
+MAX_KNOCK_TIME = 4
 KNOCK_NUM_TRIGGER = 3 #number of sequential knocks to detect the knock
-KNOCK_THRESHOLD = 6900 #thresholding
-ALARM_THRESHOLD = 8000
+KNOCK_THRESHOLD = 7000 #thresholding
+ALARM_THRESHOLD = 3000
 
 START_TIME = time.time() #get time when program starts
 
+class track:
+    def __init__(self):
+        self.knock = read("knockx3_trim.wav")[1] 
+        self.alarm = read("radar_trim.wav")[1]
+        self.num_of_knocks = 0
+        self.num_of_alarm_det = 0
+        self.first_knock = time.time()
+        self.first_alarm = time.time()
+    
 def norm_correlate(stream, template):
-	'''
-	stream: array of audio samples from real-time
-	template: array of audio samples that is to be searched for in stream
-	'''
-	template = (template - np.mean(template,axis=0))/template.std() #normalize samples
-	corr = correlate(stream, template, mode = 'full', method = 'fft')#execute normalized cross correlation
-	return corr
-	
+    '''
+    stream: array of audio samples from real-time
+    template: array of audio samples that is to be searched for in stream
+    '''
+    template = (template - np.mean(template,axis=0))/template.std() #normalize samples
+    corr = correlate(stream, template, mode = 'full', method = 'fft')#execute normalized cross correlation
+    return corr
+    
 def sequential_knocks():
-	'''
-	To be called when a single knock is detected
-	Keeps track of the time gap between knocks and then triggers
-	'''
-	global num_of_knocks
-	global last_knock_time
-	
-	#resets number of knocks if too much time has passed since the last knock
-	if time.time()-last_knock_time > MAX_KNOCK_TIME:
-		num_of_knocks = 0
-	
-	num_of_knocks+=1 #increments number of knocks in sequence
-	last_knock_time = time.time() #logs down when knock is detected
-	print(str(num_of_knocks)) 
+    '''
+    To be called when a single knock is detected
+    Keeps track of the time gap between knocks and then triggers
+    '''
+    
+    #resets number of knocks if too much time has passed since the first knock
+    if time.time()-glob.first_knock > MAX_KNOCK_TIME:
+        glob.num_of_knocks = 0
+        glob.first_knock = time.time()
+    
+    glob.num_of_knocks+=1 #increments number of knocks in sequence
+    #print(str(num_of_knocks)) 
 
-	#if number of sequential knocks reaches limit
-	if num_of_knocks == KNOCK_NUM_TRIGGER:
-		print ("knocks detected at " + str(time.time()-START_TIME) + " seconds")
-		num_of_knocks = 0
+    #if number of sequential knocks reaches limit
+    if glob.num_of_knocks == KNOCK_NUM_TRIGGER:
+        print ("knocks detected at " + str(glob.first_knock-START_TIME) + " seconds")
 
 def alarm_det():
-	if time.time()-last_alarm_time > 5:
-		print ("alarm detected at "+ str(time.time()-START_TIME) + " seconds")
+
+    if time.time()-glob.first_alarm > 2:
+        glob.num_of_alarm_det = 0
+        glob.first_alarm = time.time()
+    
+
+    glob.num_of_alarm_det += 1
+    #print(str(num_of_alarm_det))
+
+
+    if glob.num_of_alarm_det == 3:
+        print ("alarm detected at "+ str(glob.first_alarm-START_TIME) + " seconds")
+
 
 def callback(in_data, frame_count, time_info, status):
-	global knock
-	#global alarm
-	
-	stream = np.fromstring(in_data, dtype = np.int16) #read samples from stream
-	stream = stream.reshape(CHUNK,-1) #reshape into numpy array for specified number of channels
-	stream = (stream - np.mean(stream,axis=0))/stream.std() #normalize stream samples
-	knock_corr = norm_correlate(stream, knock) 
-	#alarm_corr = norm_correlate(stream, alarm)
-	#print (np.amax(knock_corr))
-	if (np.amax(knock_corr))>KNOCK_THRESHOLD:  #thresholding the detection
-		sequential_knocks()
-	#if (np.amax(alarm_corr))>ALARM_THRESHOLD:
-		#alarm_det()
-		
-	
-	return None, pyaudio.paContinue
+    global knock
+    global alarm
+    
+    stream = np.fromstring(in_data, dtype = np.int16) #read samples from stream
+    stream = stream.reshape(CHUNK,-1) #reshape into numpy array for specified number of channels
+    stream = (stream - np.mean(stream,axis=0))/stream.std() #normalize stream samples
+    knock_corr = norm_correlate(stream, glob.knock) 
+    alarm_corr = norm_correlate(stream, glob.alarm)
+      
+    if (np.amax(knock_corr))>KNOCK_THRESHOLD:  #thresholding the detection
+        sequential_knocks()
+    if (np.amax(alarm_corr))>ALARM_THRESHOLD:
+        alarm_det()
+        #print (np.amax(alarm_corr))
+    return None, pyaudio.paContinue
 
-p = pyaudio.PyAudio()
+def main():
+    p = pyaudio.PyAudio()
+    
+    print (glob.num_of_knocks)
+    #open stream
+    stream = p.open(format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK,
+        stream_callback = callback)
 
-#read the audio templates
-knock = read("knockx3.wav")[1] 
-#alarm = read("alarm.wav")[1]
+    print("* recording")
 
-num_of_knocks = 0
-last_knock_time = time.time()
+    stream.start_stream()
 
-#open stream
-stream = p.open(format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK,
-	stream_callback = callback)
-
-print("* recording")
-
-stream.start_stream()
-
-#never stop the stream
-while stream.is_active():
-	time.sleep(0.1)
+    #never stop the stream
+    while stream.is_active():
+        time.sleep(0.1)
 
 
-print("* done recording")
+    print("* done recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+glob = track()
+main()
 
